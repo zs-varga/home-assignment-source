3136c98efc2c6c58e9d5c572ac18688260798975
Zsolt Varga <zsolt.varga@supercharge.io>
2026-01-12 18:04:51 +0100

    Add build obfuscation and minification configuration
    - Configure Terser for aggressive JavaScript minification
    - Enable console removal and variable mangling
    - Configure CSS minification
    - Remove all comments from output
    - Install terser dependency

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,24 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        pure_funcs: ['console.log', 'console.info', 'console.debug'],
+      },
+      mangle: true,
+      format: {
+        comments: false,
+      },
+    },
+    cssMinify: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
 })
